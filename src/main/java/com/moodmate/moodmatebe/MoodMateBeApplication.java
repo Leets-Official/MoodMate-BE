d2f618366cd225e9841a74e075256ab52f98cda9
leeseobin00 <leeseobin000709@gmail.com>
2023-11-06 23:36:22 +0900

    ✨[feat]: 프로젝트 초기 설정

---
+++ src/main/java/com/moodmate/moodmatebe/MoodMateBeApplication.java
@@ -0,0 +1,13 @@
+package com.moodmate.moodmatebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodMateBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodMateBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moodmate/moodmatebe/MoodMateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodmate.moodmatebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodMateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
