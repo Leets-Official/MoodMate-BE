88b588e1b3b2b89a3fe1edc38a3de67a3070d9a2
leeseobin00 <leeseobin000709@gmail.com>
2023-11-09 02:38:46 +0900

    ✨[feat]: ErrorCode 열거형 클래스 작성
    * 서버 오류 코드를 정의합니다.

---
+++ src/main/java/com/moodmate/moodmatebe/global/error/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.moodmate.moodmatebe.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
