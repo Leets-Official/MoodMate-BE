8f15a7277a4a75643c6534f6e2f751cfd73567d0
jiixon <jr0107380@gmail.com>
2023-11-14 15:18:07 +0900

    :sparkles:[feat]:메세지 브로커에 관한 설정

--- src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.moodmate.moodmatebe.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,5 +13,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/chat")
                 .setAllowedOrigins("*")
                 .withSockJS();
+    }
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        //pub 로 데이터를 받으면 이곳을 한번 거쳐서 URI 만 MessageMapping 에 매핑
+        registry.setApplicationDestinationPrefixes("/pub");
+        //해당 주소를 구독하고 있는 클라이언트에게 메시지 전달
+        registry.enableSimpleBroker("/sub");
     }
 }
