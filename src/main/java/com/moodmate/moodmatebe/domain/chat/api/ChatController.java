1fd2ebeaaf35650e61aba2048cc14da03230cf24
jiixon <jr0107380@gmail.com>
2023-11-15 22:09:08 +0900

    :recycle:[refactor]: ChatMessageDto로 이름 변경

--- src/main/java/com/moodmate/moodmatebe/domain/chat/api/ChatController.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/api/ChatController.java
@@ -1,5 +1,6 @@
 package com.moodmate.moodmatebe.domain.chat.api;
 
+import com.moodmate.moodmatebe.domain.chat.dto.ChatMessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public ChatMessage handleChatMessage(ChatMessage message) {
+    public ChatMessageDto handleChatMessage(ChatMessageDto message) {
         return message;
     }
 }
