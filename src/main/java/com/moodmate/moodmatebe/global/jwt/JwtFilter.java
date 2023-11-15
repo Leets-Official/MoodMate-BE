920942a9b07ecee3a11383edbd25caac6b5cbc25
leeseobin00 <leeseobin000709@gmail.com>
2023-11-16 05:10:08 +0900

    ✨[feat]: JwtFilter 클래스 추가
    * `OncePerRequestFilter`를 상속하여 각 요청에 대하여 한 번만 실행
    * JWT 토큰을 추출
    * 토큰이 존재하면 유효성 검증 및 사용자 인증
    * Authorization 헤더에서 토큰을 추출

---
+++ src/main/java/com/moodmate/moodmatebe/global/jwt/JwtFilter.java
@@ -0,0 +1,37 @@
+package com.moodmate.moodmatebe.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+        if (token != null) {
+            jwtProvider.validateToken(token, false);
+            Authentication authentication = this.jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.split(" ")[1];
+        }
+        return null;
+    }
+}
