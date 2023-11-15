4f760b648abd5196bd68bee46690c1033e9ecb87
jiixon <jr0107380@gmail.com>
2023-11-15 16:15:33 +0900

    :sparkles:[feat]: Lettuce를 사용하여 Redis와의 연결을 설정

--- src/main/java/com/moodmate/moodmatebe/global/config/RedisConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/RedisConfig.java
@@ -1,2 +1,26 @@
-package com.moodmate.moodmatebe.global.config;public class RedisConfig {
+package com.moodmate.moodmatebe.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    //Lettuce를 사용하여 Redis와의 연결을 설정
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host,port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
 }
