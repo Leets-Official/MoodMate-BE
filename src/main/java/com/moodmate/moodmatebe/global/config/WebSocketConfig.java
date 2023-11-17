ecab57bff94af56e6116d2c3d64a78d18baaf3ab
jiixon <jr0107380@gmail.com>
2023-11-18 00:58:34 +0900

    :recycle:[refactor]: 사용하지않은 어노테이션 제거

--- src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.moodmate.moodmatebe.global.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,7 +7,6 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@Slf4j
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
