0525fb96dd184d1c11d3f7b1fc35c9e2e786d28d
jiixon <jr0107380@gmail.com>
2023-11-17 14:58:09 +0900

    :bug:[fix]: localhost:8080/를 허용하는 CORS 설정 변경

--- src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.moodmate.moodmatebe.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -7,19 +8,23 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@Slf4j
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("localhost:8080/")
                 .withSockJS();
+        log.info("연결!!");
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         //pub 로 데이터를 받으면 이곳을 한번 거쳐서 URI 만 MessageMapping 에 매핑
         registry.setApplicationDestinationPrefixes("/pub");
+        log.info("pub");
         //해당 주소를 구독하고 있는 클라이언트에게 메시지 전달
         registry.enableSimpleBroker("/sub");
+        log.info("pub");
     }
 }
