4248695c1e1f2f150f8bb6d8938c8ca21f2da46e
jiixon <jr0107380@gmail.com>
2023-11-18 00:47:28 +0900

    :recycle:[refactor]: websocketconfig 불필요한 로그제거

--- src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
@@ -14,17 +14,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("localhost:8080/")
+                .setAllowedOrigins("localhost:8080")
                 .withSockJS();
-        log.info("연결!!");
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         //pub 로 데이터를 받으면 이곳을 한번 거쳐서 URI 만 MessageMapping 에 매핑
         registry.setApplicationDestinationPrefixes("/pub");
-        log.info("pub");
         //해당 주소를 구독하고 있는 클라이언트에게 메시지 전달
         registry.enableSimpleBroker("/sub");
-        log.info("pub");
     }
 }
