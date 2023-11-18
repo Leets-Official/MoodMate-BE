b461ff88286a4132f17c73bc5c6b9248a925db50
jiixon <jr0107380@gmail.com>
2023-11-18 21:54:27 +0900

    :sparkles:[feat]: redis 예외처리 코드 추가

--- src/main/java/com/moodmate/moodmatebe/global/error/ErrorCode.java
+++ src/main/java/com/moodmate/moodmatebe/global/error/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다.");
+    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
+    FAILED_CONNECT_REDIS(500,"FAILED_CONNECT_REDIS","레디스 연결에 실패했습니다."),
+    FAILED_SERIALIZE_DATA(500,"FAILED_SERIALIZE_DATA","데이터 직렬화에 실패했습니다.");
 
     private final int httpStatus;
     private final String code;
