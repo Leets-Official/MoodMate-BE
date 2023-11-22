18c967748a209b5de94d6e938ea0894b3f650d94
jiixon <jr0107380@gmail.com>
2023-11-23 03:35:47 +0900

    :sparkles:[feat]: 채팅메시지 Redis와 DB에 저장하는 기능구현

---
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatService.java
@@ -0,0 +1,40 @@
+package com.moodmate.moodmatebe.domain.chat.application;
+
+import com.moodmate.moodmatebe.domain.chat.domain.ChatMessage;
+import com.moodmate.moodmatebe.domain.chat.domain.ChatRoom;
+import com.moodmate.moodmatebe.domain.chat.dto.ChatMessageDto;
+import com.moodmate.moodmatebe.domain.chat.repository.MessageRepository;
+import com.moodmate.moodmatebe.domain.chat.repository.RoomRepository;
+import com.moodmate.moodmatebe.domain.user.domain.User;
+import com.moodmate.moodmatebe.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+    private final RedisTemplate<String, ChatMessageDto> chatRedistemplate;
+    private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
+    private final MessageRepository messageRepository;
+
+    public void saveMessage(ChatMessageDto chatMessageDto) throws Exception {
+        Optional<ChatRoom> byId = roomRepository.findById(chatMessageDto.getRoomNo());
+        Optional<User> userId = userRepository.findById(chatMessageDto.getUserNo());
+        if (byId.isPresent()) {
+            // ChatRoom이 존재하는 경우
+            ChatRoom chatRoom = byId.get();
+            ChatMessage chatMessage = new ChatMessage(chatRoom, userId.get(), true, chatMessageDto.getContent(), LocalDateTime.now());
+            messageRepository.save(chatMessage);
+            chatRedistemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
+            chatRedistemplate.opsForList().rightPush(chatMessageDto.getRoomNo().toString(), chatMessageDto);
+        } else {
+            throw new Exception("ChatRoom NOT FOUND!");
+        }
+    }
+}
