ca4436402b0a570845d214524b634e6fd3457927
jiixon <jr0107380@gmail.com>
2023-11-23 03:15:34 +0900

    :sparkles:[feat]: 채팅방입장과 함께 리스너 연동하는 service 구현

---
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatRoomService.java
@@ -0,0 +1,19 @@
+package com.moodmate.moodmatebe.domain.chat.application;
+
+import com.moodmate.moodmatebe.domain.chat.redis.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+    private final RedisMessageListenerContainer redisMessageListener;
+    private final RedisSubscriber redisSubscriber;
+
+    public void enterMessageRoom(Long roomId) {
+        ChannelTopic topic = new ChannelTopic("/sub/chat/" + roomId);
+        redisMessageListener.addMessageListener(redisSubscriber, topic);
+    }
+}
