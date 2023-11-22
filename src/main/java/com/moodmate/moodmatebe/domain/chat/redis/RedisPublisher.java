d0351da276bf4db64e89988dc982d6fcbc37deef
jiixon <jr0107380@gmail.com>
2023-11-22 15:05:43 +0900

    :sparkles:[feat]: Redis 채널에 메시지를 발행하도록 publisher 설정

---
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/redis/RedisPublisher.java
@@ -0,0 +1,17 @@
+package com.moodmate.moodmatebe.domain.chat.redis;
+
+import com.moodmate.moodmatebe.domain.chat.dto.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessageDto messageDto) {
+        redisTemplate.convertAndSend(topic.getTopic(), messageDto);
+    }
+}
