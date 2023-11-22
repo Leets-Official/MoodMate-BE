ae633741cff596e8378778b0f5ef5bef5b40d602
jiixon <jr0107380@gmail.com>
2023-11-23 03:33:29 +0900

    :sparkles:[feat]: UserRepository 생성

---
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.moodmate.moodmatebe.domain.user.repository;
+
+import com.moodmate.moodmatebe.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long userNo);
+}
