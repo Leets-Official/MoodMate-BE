25242adcfcd577f73b4d86b7f678ae229a3f5fd7
jiixon <jr0107380@gmail.com>
2023-11-23 17:20:59 +0900

    :recycle:[refactor]: 필요없는 resource 어노테이션 제거

--- src/main/java/com/moodmate/moodmatebe/domain/chat/redis/RedisPublisher.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/redis/RedisPublisher.java
@@ -1,7 +1,6 @@
 package com.moodmate.moodmatebe.domain.chat.redis;
 
 import com.moodmate.moodmatebe.domain.chat.dto.ChatMessageDto;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
-    @Resource(name = "redisTemplate")
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void publish(ChannelTopic topic, ChatMessageDto messageDto) {
