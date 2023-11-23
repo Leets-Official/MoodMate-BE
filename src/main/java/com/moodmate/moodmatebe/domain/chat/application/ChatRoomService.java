ff207d24ad3ae6712b869faf01cdaa8e8d42bd52
jiixon <jr0107380@gmail.com>
2023-11-23 16:42:08 +0900

    :sparkles:[feat]: ConnectionException, ChatRoomNotFoundException 예외처리

--- src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatRoomService.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatRoomService.java
@@ -1,6 +1,8 @@
 package com.moodmate.moodmatebe.domain.chat.application;
 
+import com.moodmate.moodmatebe.domain.chat.exception.ChatRoomNotFoundException;
 import com.moodmate.moodmatebe.domain.chat.redis.RedisSubscriber;
+import com.moodmate.moodmatebe.domain.chat.redis.exception.ConnectionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -12,8 +14,16 @@ public class ChatRoomService {
     private final RedisMessageListenerContainer redisMessageListener;
     private final RedisSubscriber redisSubscriber;
 
-    public void enterChatRoom(Long roomId) {
+    public void enterChatRoom(Long roomId) throws ChatRoomNotFoundException, ConnectionException {
         ChannelTopic topic = new ChannelTopic("/sub/chat/" + roomId);
-        redisMessageListener.addMessageListener(redisSubscriber, topic);
+        try{
+            redisMessageListener.addMessageListener(redisSubscriber, topic);
+        }catch (Exception e){
+            if(e.getCause() instanceof ChatRoomNotFoundException){
+                throw new ChatRoomNotFoundException();
+            }else if (e.getCause() instanceof ConnectionException){
+                throw new ConnectionException();
+            }
+        }
     }
 }
