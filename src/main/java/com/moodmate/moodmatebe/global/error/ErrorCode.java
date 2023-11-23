d81f7dc3d22168221481ea5a03849fa3f8751c09
jiixon <jr0107380@gmail.com>
2023-11-23 16:19:19 +0900

    :sparkles:[feat]: chatRoom not found exception 구현

--- src/main/java/com/moodmate/moodmatebe/global/error/ErrorCode.java
+++ src/main/java/com/moodmate/moodmatebe/global/error/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
     FAILED_CONNECT_REDIS(500,"FAILED_CONNECT_REDIS","레디스 연결에 실패했습니다."),
-    FAILED_SERIALIZE_DATA(500,"FAILED_SERIALIZE_DATA","데이터 직렬화에 실패했습니다.");
-
+    FAILED_SERIALIZE_DATA(500,"FAILED_SERIALIZE_DATA","데이터 직렬화에 실패했습니다."),
+    CHAT_ROOM_NOT_FOUND(404,"CHAT_ROOM_NOT_FOUND","채팅방을 찾을 수 없습니다.");
     private final int httpStatus;
     private final String code;
     private final String message;
