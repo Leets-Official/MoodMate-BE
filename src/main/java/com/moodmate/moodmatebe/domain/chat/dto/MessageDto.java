31cecbd4b4e89c7d9301be362e8a6deea3439d49
jiixon <jr0107380@gmail.com>
2023-11-27 00:57:29 +0900

    :recycle:[refactor]: messageId 저장을 위해 Setter선언

--- src/main/java/com/moodmate/moodmatebe/domain/chat/dto/MessageDto.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/dto/MessageDto.java
@@ -2,15 +2,17 @@ package com.moodmate.moodmatebe.domain.chat.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class MessageDto {
     private Long messageId;
     private String content;
-    private Long senderId;
-    private LocalDateTime sendTime;
+    private Long userId;
+    private LocalDateTime createdAt;
     private Boolean isRead;
 }
