172b4556bf867e1c83b1fd90788469a1b9885510
jiixon <jr0107380@gmail.com>
2023-11-27 01:03:58 +0900

    :recycle:[refactor]: RedisChatMessageDto설정 추가

--- src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatService.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/application/ChatService.java
@@ -4,18 +4,21 @@ import com.moodmate.moodmatebe.domain.chat.domain.ChatMessage;
 import com.moodmate.moodmatebe.domain.chat.domain.ChatRoom;
 import com.moodmate.moodmatebe.domain.chat.dto.RedisChatMessageDto;
 import com.moodmate.moodmatebe.domain.chat.exception.ChatRoomNotFoundException;
+import com.moodmate.moodmatebe.domain.chat.redis.RedisMessageIdGenerator;
 import com.moodmate.moodmatebe.domain.chat.repository.MessageRepository;
 import com.moodmate.moodmatebe.domain.chat.repository.RoomRepository;
 import com.moodmate.moodmatebe.domain.user.domain.User;
 import com.moodmate.moodmatebe.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,8 @@ public class ChatService {
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
     private final MessageRepository messageRepository;
+    private final RedisMessageIdGenerator redisMessageIdGenerator;
+
 
     @Transactional
     public void saveMessage(RedisChatMessageDto chatMessageDto){
@@ -31,8 +36,10 @@ public class ChatService {
         Optional<User> userId = userRepository.findById(chatMessageDto.getUserId());
         if (roomId.isPresent()) {
             ChatRoom chatRoom = roomId.get();
-            ChatMessage chatMessage = new ChatMessage(chatRoom, userId.get(), true, chatMessageDto.getContent(), LocalDateTime.now());
+            ChatMessage chatMessage = new ChatMessage(chatRoom, userId.get(), true, chatMessageDto.getContent(),LocalDateTime.now());
             messageRepository.save(chatMessage);
+            Long messageId = redisMessageIdGenerator.generateUniqueId(chatMessageDto.getRoomId().toString());
+            chatMessageDto.setMessageId(messageId);
             chatRedistemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
             chatRedistemplate.opsForList().rightPush(chatMessageDto.getRoomId().toString(), chatMessageDto);
         } else {
