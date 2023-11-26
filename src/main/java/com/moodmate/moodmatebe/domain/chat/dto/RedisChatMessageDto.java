1749ca504b100c6b7b130ca4e373c8870efa0d07
jiixon <jr0107380@gmail.com>
2023-11-27 00:59:45 +0900

    :sparkles:[feat]: Redis에 저장할 Dto 따로 설정

--- src/main/java/com/moodmate/moodmatebe/domain/chat/dto/RedisChatMessageDto.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/dto/RedisChatMessageDto.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
+@Builder
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RedisChatMessageDto {
+    private Long messageId;
     private Long userId;
     private Long roomId;
     private String content;
@@ -19,4 +24,14 @@ public class RedisChatMessageDto {
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdAt;
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("userId", userId);
+        map.put("roomId", roomId);
+        map.put("content", content);
+        map.put("isRead", isRead);
+        map.put("createdAt", createdAt);
+        return map;
+    }
 }
