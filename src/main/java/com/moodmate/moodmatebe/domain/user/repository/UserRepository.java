d01c0c8d468393f4ce4757a45bdda6f27951aa3d
leeseobin00 <leeseobin000709@gmail.com>
2023-11-27 22:44:19 +0900

    ✨[feat]: findByUserEmail 메서드 추가
    * UserRepository에 findByUserEmail 메서드를 추가했습니다.

--- src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
+
+    Optional<User> findByUserEmail(String userEmail);
 }
