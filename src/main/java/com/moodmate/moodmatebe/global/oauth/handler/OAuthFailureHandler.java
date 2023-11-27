c9ea72134382372468b6761a9b54a45305d430ab
leeseobin00 <leeseobin000709@gmail.com>
2023-11-27 23:18:43 +0900

    ✨[feat]: OAuthFailureHandler 추가
    * OAuth 로그인 실패 시 처리를 담당하는 `OAuthFailureHandler`를 추가했습니다.
    * 오류의 로그를 남깁니다.

---
+++ src/main/java/com/moodmate/moodmatebe/global/oauth/handler/OAuthFailureHandler.java
@@ -0,0 +1,22 @@
+package com.moodmate.moodmatebe.global.oauth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        logger.debug("OAuth login failed: " + exception.getMessage());
+        exception.printStackTrace();
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
