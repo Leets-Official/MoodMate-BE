32178910714c7e0cfba15a067e7e25a08114f3ef
MinhoJJang <ahchjang@naver.com>
2023-11-28 01:09:42 +0900

    :sparkles:[feat]: RoomRepository 작성

--- src/main/java/com/moodmate/moodmatebe/domain/chat/repository/RoomRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/chat/repository/RoomRepository.java
@@ -2,9 +2,13 @@ package com.moodmate.moodmatebe.domain.chat.repository;
 
 import com.moodmate.moodmatebe.domain.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByRoomId(Long roomId);
+    @Query("SELECT cr FROM chat_room cr WHERE (cr.user1.userId = :userId OR cr.user2.userId = :userId) AND cr.roomActive = true ORDER BY cr.createdAt DESC")
+    Optional<ChatRoom> findLatestActiveByUserId(@Param("userId") Long userId);
 }
