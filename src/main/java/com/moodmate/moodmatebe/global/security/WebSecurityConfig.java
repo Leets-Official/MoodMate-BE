0aad9ee4440f635c85c039964abe7272997abd7e
leeseobin00 <leeseobin000709@gmail.com>
2023-11-28 03:55:18 +0900

    ✨[feat]: WebSecurityConfig 업데이트
    * Oauth와 관련된 Config를 추가하였습니다.
    * 인증 엔드포인트, 리다이렉션 엔드포인트, 유저 서비스, 성공 핸들러, 실패 핸들러 등을 설정하였습니다

--- src/main/java/com/moodmate/moodmatebe/global/security/WebSecurityConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/security/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.moodmate.moodmatebe.global.security;
 
+import com.moodmate.moodmatebe.global.filter.ExceptionHandleFilter;
 import com.moodmate.moodmatebe.global.jwt.JwtFilter;
 import com.moodmate.moodmatebe.global.jwt.JwtProvider;
+import com.moodmate.moodmatebe.global.oauth.application.OAuthDetailService;
+import com.moodmate.moodmatebe.global.oauth.handler.OAuthFailureHandler;
+import com.moodmate.moodmatebe.global.oauth.handler.OAuthSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +23,9 @@ import org.springframework.web.cors.CorsUtils;
 @EnableWebSecurity
 public class WebSecurityConfig {
     private final JwtProvider jwtProvider;
+    private final OAuthDetailService oAuthDetailService;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
+    private final OAuthFailureHandler oAuthFailureHandler;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -29,7 +35,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors()
+                .and()
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
@@ -44,6 +51,22 @@ public class WebSecurityConfig {
                 .requestMatchers("/**").permitAll()
 
                 .anyRequest().authenticated()
+                .and()
+
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth/login")
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth/callback/**")
+                .and()
+                .userInfoEndpoint()
+                .userService(oAuthDetailService)
+                .and()
+                .successHandler(oAuthSuccessHandler)
+                .failureHandler(oAuthFailureHandler)
+                .and()
+
                 .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new ExceptionHandleFilter(), JwtFilter.class);
 
