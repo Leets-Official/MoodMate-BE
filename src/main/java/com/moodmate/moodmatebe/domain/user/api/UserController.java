d145d44e6984cd309effd27b9ef0ac5fcfaea245
leeseobin00 <leeseobin000709@gmail.com>
2023-11-28 22:12:08 +0900

    📝[docs]: Swagger 문서 수정

--- src/main/java/com/moodmate/moodmatebe/domain/user/api/UserController.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/api/UserController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Tag(name = "사용자")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/users")
