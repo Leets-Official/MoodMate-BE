fdbc329aad1abcefa50446d4ef70587ab46dfdbb
leeseobin00 <leeseobin000709@gmail.com>
2023-12-01 22:48:25 +0900

    ✨[feat]: PreferRepository 생성

--- src/main/java/com/moodmate/moodmatebe/domain/user/repository/PreferRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/PreferRepository.java
@@ -1,6 +1,7 @@
 package com.moodmate.moodmatebe.domain.user.repository;
 
 import com.moodmate.moodmatebe.domain.user.domain.Prefer;
+import com.moodmate.moodmatebe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.Optional;
 
 public interface PreferRepository extends JpaRepository<Prefer, Long> {
     Optional<Prefer> findById(Long preferId);
+
     List<Prefer> findAll();
+
+    Optional<Prefer> findByUser(User user);
 }
