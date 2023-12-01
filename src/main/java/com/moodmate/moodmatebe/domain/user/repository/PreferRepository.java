d313f207a6bdc6eb18ceaa0c2b6ad37f90faf0c0
jiixon <jr0107380@gmail.com>
2023-12-02 00:13:27 +0900

    :sparkles:[feat]: userId로 preferMood 찾는 JPA 작성

--- src/main/java/com/moodmate/moodmatebe/domain/user/repository/PreferRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/PreferRepository.java
@@ -2,11 +2,12 @@ package com.moodmate.moodmatebe.domain.user.repository;
 
 import com.moodmate.moodmatebe.domain.user.domain.Prefer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PreferRepository extends JpaRepository<Prefer, Long> {
-    Optional<Prefer> findById(Long preferId);
-    List<Prefer> findAll();
+    @Query("SELECT p.preferMood FROM prefer p WHERE p.user.userId = :userId")
+    Optional<String> findPreferMoodByUserId(@Param("userId") Long userId);
 }
