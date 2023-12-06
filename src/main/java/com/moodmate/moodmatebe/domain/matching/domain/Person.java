41be79f62d991edbc8f83d540cb9aee2e714d76c
DongHoon Lee <dhl1924@naver.com>
2023-12-06 10:19:50 +0900

    :sparkles:[feat]: Person 클래스 생성
    - User Entity와 Prefer Entity의 객체를 이용해 Person의 필드를 생성자에서 초기화 합니다.

---
+++ src/main/java/com/moodmate/moodmatebe/domain/matching/domain/Person.java
@@ -0,0 +1,37 @@
+package com.moodmate.moodmatebe.domain.matching.domain;
+
+import com.moodmate.moodmatebe.domain.user.domain.Gender;
+import com.moodmate.moodmatebe.domain.user.domain.Prefer;
+import com.moodmate.moodmatebe.domain.user.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private Gender gender;
+    private User user;
+    private Prefer prefer;
+    private String name;  // 이름
+    private String mood;  // 선호하는 무드
+    private List<String> preferences;  // 선호도 목록
+    private int minYear;  // 선호하는 최소 나이
+    private int maxYear;  // 선호하는 최대 나이
+    private int year; // 나이
+    private String department; // 학과
+    private boolean dontCareSameDepartment; // 같은 학과 선호 여부, 기본적으로 false로 초기화
+
+    public Person(User user, Prefer prefer) {
+        this.name = user.getUserNickname();
+        this.mood = prefer.getPreferMood();
+        this.minYear = prefer.getPreferYearMin();
+        this.maxYear = prefer.getPreferYearMax();
+        this.year = user.getYear();
+        this.department = user.getUserDepartment();
+        this.dontCareSameDepartment = prefer.isPreferDepartmentPossible();
+        this.preferences = new ArrayList<>();
+    }
+}
