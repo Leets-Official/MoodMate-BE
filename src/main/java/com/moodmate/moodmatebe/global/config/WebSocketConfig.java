0aba0c84d0e28149c46056f449c111b33e5f7051
jiixon <jr0107380@gmail.com>
2023-12-09 00:16:14 +0900

    :ambulance:[HOTFIX]: socket 로그추가

--- src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("https://www.moodmate.site")
                 .withSockJS();
     }
     @Override
