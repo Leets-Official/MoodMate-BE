3dcede69c817432522281bdb9d973abd54b99559
jiixon <jr0107380@gmail.com>
2023-12-09 21:40:48 +0900

    :ambulance:[HOTFIX]: stomphandler수정

--- src/main/java/com/moodmate/moodmatebe/global/config/handler/StompHandler.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/handler/StompHandler.java
@@ -1,30 +1,42 @@
 package com.moodmate.moodmatebe.global.config.handler;
 
 import com.moodmate.moodmatebe.global.jwt.JwtProvider;
-import lombok.RequiredArgsConstructor;
+import com.moodmate.moodmatebe.global.jwt.exception.ExpiredTokenException;
+import com.moodmate.moodmatebe.global.jwt.exception.InvalidTokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
+
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class StompHandler implements ChannelInterceptor {
     private final JwtProvider jwtProvider;
+    public StompHandler(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
 
     // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-
-        // CONNECT 요청이면서 'Authorization' 헤더가 있는 경우에만 토큰 검증을 수행합니다.
-        if (StompCommand.CONNECT == accessor.getCommand() && accessor.getFirstNativeHeader("Authorization") != null) {
-            String token = accessor.getFirstNativeHeader("Authorization");
-            jwtProvider.validateToken(token, false);
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            String authorizationHeader = accessor.getFirstNativeHeader("Authorization");
+            if (authorizationHeader != null) {
+                try {
+                    jwtProvider.validateToken(authorizationHeader, false);
+                } catch (ExpiredTokenException | InvalidTokenException e) {
+                    throw new AccessDeniedException("Access Denied");
+                }
+            } else {
+                throw new AccessDeniedException("Access Denied");
+            }
         }
         return message;
     }
