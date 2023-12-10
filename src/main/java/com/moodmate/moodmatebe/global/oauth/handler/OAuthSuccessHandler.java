303476ff12034f081c1f08110adad3455529f7f8
leeseobin00 <leeseobin000709@gmail.com>
2023-12-10 14:35:56 +0900

    🔥[style]: 불필요한 import 구문 삭제

--- src/main/java/com/moodmate/moodmatebe/global/oauth/handler/OAuthSuccessHandler.java
+++ src/main/java/com/moodmate/moodmatebe/global/oauth/handler/OAuthSuccessHandler.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
