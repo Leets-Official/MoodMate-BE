939476b3902694ff1adba632b2cc7b096d3de7ab
leeseobin00 <leeseobin000709@gmail.com>
2023-12-16 02:40:18 +0900

    🚑[HOTFIX]: 쿠키 전송 제거

--- src/main/java/com/moodmate/moodmatebe/global/oauth/handler/OAuthSuccessHandler.java
+++ src/main/java/com/moodmate/moodmatebe/global/oauth/handler/OAuthSuccessHandler.java
@@ -47,20 +47,6 @@ public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
             String accessToken = jwtProvider.generateToken(id, email, role, false);
             String refreshToken = jwtProvider.generateToken(id, email, role, true);
 
-            Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
-            accessTokenCookie.setHttpOnly(false);
-            accessTokenCookie.setSecure(false);
-            accessTokenCookie.setMaxAge(60 * 60 * 2);
-            accessTokenCookie.setPath("/");
-            response.addCookie(accessTokenCookie);
-
-            Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-            refreshTokenCookie.setHttpOnly(false);
-            refreshTokenCookie.setSecure(false);
-            refreshTokenCookie.setPath("/");
-            refreshTokenCookie.setMaxAge(60 * 60 * 24 * 14);
-            response.addCookie(refreshTokenCookie);
-
 //            String redirectUrlWithToken = buildRedirectUrlWithToken(redirectUrl, accessToken);
 //            getRedirectStrategy().sendRedirect(request, response, redirectUrlWithToken);
 
