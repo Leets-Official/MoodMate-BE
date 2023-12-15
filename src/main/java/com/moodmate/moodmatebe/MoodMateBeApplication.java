1dd0a2d140a92ee28b03307d9961bb1ae9009729
DongHoon Lee <dhl1924@naver.com>
2023-12-15 14:17:52 +0900

    :sparkles:[feat]: @EnableScheduling 어노테이션 적용하여 시간 자동화 기능 구현

--- src/main/java/com/moodmate/moodmatebe/MoodMateBeApplication.java
+++ src/main/java/com/moodmate/moodmatebe/MoodMateBeApplication.java
@@ -3,9 +3,11 @@ package com.moodmate.moodmatebe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class MoodMateBeApplication {
 
     public static void main(String[] args) {
