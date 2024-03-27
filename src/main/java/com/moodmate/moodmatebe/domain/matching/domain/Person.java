4723eb4ef2a77feeda4666d3d33c1fec6e9d1df9
DongHoon Lee <dhl1924@naver.com>
2024-03-27 22:29:13 +0900

    :bulb:[chore]: 주석 삭제

--- src/main/java/com/moodmate/moodmatebe/domain/matching/domain/Person.java
+++ src/main/java/com/moodmate/moodmatebe/domain/matching/domain/Person.java
@@ -15,14 +15,14 @@ public class Person {
     private Gender gender;
     private User user;
     private Prefer prefer;
-    private String name;  // 이름
-    private String mood;  // 선호하는 무드
-    private List<String> preferences;  // 선호도 목록
-    private int minYear;  // 선호하는 최소 나이
-    private int maxYear;  // 선호하는 최대 나이
-    private int year; // 나이
-    private String department; // 학과
-    private boolean dontCareSameDepartment; // 같은 학과 선호 여부, 기본적으로 false로 초기화
+    private String name;
+    private String mood;
+    private List<String> preferences;
+    private int minYear;
+    private int maxYear;
+    private int year;
+    private String department;
+    private boolean dontCareSameDepartment;
 
     public Person(User user, Prefer prefer) {
         this.user = user;
