afb4736cfe312c4cb2e1f09c00cd5ce22c96406e
MinhoJJang <ahchjang@naver.com>
2024-04-13 18:02:32 +0900

    :wrench:[refactor] change baseUrl to variable

--- src/main/java/com/moodmate/moodmatebe/global/config/ClientConfig.java
+++ src/main/java/com/moodmate/moodmatebe/global/config/ClientConfig.java
@@ -1,5 +1,6 @@
 package com.moodmate.moodmatebe.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -15,11 +16,13 @@ public class ClientConfig {
         return new RestTemplate();
     }
 
+    @Value("${app.baseUrl}")
+    private String baseUrl;
     @Bean
     public WebClient webClient() {
         return WebClient
                 .builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl(baseUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
