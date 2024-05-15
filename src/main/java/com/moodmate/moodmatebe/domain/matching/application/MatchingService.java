c5f25bc081ab321e61a3a10e1bac6ba968f528fd
DongHoon Lee <dhl1924@naver.com>
2024-05-15 22:10:26 +0900

    :recycle:[refactor]: MatchingService 가독성 개선

--- src/main/java/com/moodmate/moodmatebe/domain/matching/application/MatchingService.java
+++ src/main/java/com/moodmate/moodmatebe/domain/matching/application/MatchingService.java
@@ -33,124 +33,131 @@ public class MatchingService {
 
     @Transactional
     public void match() {
-        List<Prefer> activeMatchTrue = preferRepository.findByUserMatchActiveAndGenderTrue(Gender.MALE);
-        activeMatchTrue.addAll(preferRepository.findByUserMatchActiveAndGenderTrue(Gender.FEMALE));
-
+        List<Prefer> activeMatchTrue = getActiveMatchPreferences();
         Collections.shuffle(activeMatchTrue);
 
         for (Prefer prefer : activeMatchTrue) {
-            Person person = Person.createPerson(prefer.getUser(), prefer);
+            addPersonToGroup(prefer);
+        }
+    }
 
-            if (person.getUserProfile().gender() == Gender.MALE) {
-                men.add(Man.createMan(prefer.getUser(), prefer));
-            } else if (person.getUserProfile().gender() == Gender.FEMALE) {
-                women.add(Woman.createWoman(prefer.getUser(), prefer));
-            }
+    private List<Prefer> getActiveMatchPreferences() {
+        List<Prefer> activeMatchTrue = preferRepository.findByUserMatchActiveAndGenderTrue(Gender.MALE);
+        activeMatchTrue.addAll(preferRepository.findByUserMatchActiveAndGenderTrue(Gender.FEMALE));
+        return activeMatchTrue;
+    }
+
+    private void addPersonToGroup(Prefer prefer) {
+        Person person = Person.createPerson(prefer.getUser(), prefer);
+        if (person.getUserProfile().gender() == Gender.MALE) {
+            men.add(Man.createMan(prefer.getUser(), prefer));
+        } else if (person.getUserProfile().gender() == Gender.FEMALE) {
+            women.add(Woman.createWoman(prefer.getUser(), prefer));
         }
     }
 
     @Transactional
     public void grouping() {
-        Map<String, Man> m = convertListToMap(men);
-        Map<String, Woman> w = convertListToMap(women);
-
-        Map<String, Map<String, Man>> menGroups = groupByMood(m);
-        Map<String, Map<String, Woman>> womenGroups = groupByMood(w);
+        Map<String, Map<String, Man>> menGroups = groupByMood(convertListToMap(men));
+        Map<String, Map<String, Woman>> womenGroups = groupByMood(convertListToMap(women));
 
         for (String mood : menGroups.keySet()) {
-            Map<String, Man> menGroup = menGroups.get(mood);
-            Map<String, Woman> womenGroup = womenGroups.get(mood);
-
-            log.info("====================================");
-            log.info("{} 그룹 매칭을 시작합니다.", mood);
-            log.info("====================================");
-
-            log.debug("{} 그룹 남자:", mood);
-            for (Man man : menGroup.values()) {
-                log.info(man.getName());
-            }
-
-            log.debug("{} 그룹 여자:", mood);
-            for (Woman woman : womenGroup.values()) {
-                log.info(woman.getName());
-            }
-
-            for (Man man : menGroup.values()) {
-                List<String> mainPreferences = new ArrayList<>();
-                List<String> secondaryPreferences = new ArrayList<>();
-                List<String> thirdPreferences = new ArrayList<>();
-                Set<String> metFemalesSet = new HashSet<>();
-
-                User manUser = userRepository.findById(man.getUserProfile().userId()).orElseThrow(
-                        () -> new IllegalArgumentException("ID에 해당하는 유저가 없음"));
-                List<WhoMeet> metFemales = whoMeetRepository.findByMetUser2(manUser);
-                for (WhoMeet met : metFemales) {
-                    metFemalesSet.add(met.getMetUser1().getUserNickname());
-                }
-
-                for (Woman woman : womenGroup.values()) {
-                    String womanName = woman.getName();
-                    if (metFemalesSet.contains(womanName)) {
-                        continue;
-                    }
-
-                    if (!man.getUserPreferences().departmentPossible() && man.getUserProfile().department().equals(woman.getUserProfile().department())) {
-                        thirdPreferences.add(womanName);
-                    } else if (woman.getUserProfile().birthYear() >= man.getUserPreferences().yearMin() && woman.getUserProfile().birthYear() <= man.getUserPreferences().yearMax() ) {
-                        mainPreferences.add(womanName);
-                    } else {
-                        secondaryPreferences.add(womanName);
-                    }
-                }
-
-                mainPreferences.addAll(secondaryPreferences);
-                mainPreferences.addAll(thirdPreferences);
-                mainPreferences.addAll(metFemalesSet);
-
-                man.setPreferences(mainPreferences);
-
-                log.info("{}의 선호도 목록 : {}", man.getName(), mainPreferences);
-            }
-
-            for (Woman woman : womenGroup.values()) {
-                List<String> mainPreferences = new ArrayList<>();
-                List<String> secondaryPreferences = new ArrayList<>();
-                List<String> thirdPreferences = new ArrayList<>();
-                Set<String> metMalesSet = new HashSet<>();
-
-                User womanUser = userRepository.findById(woman.getUserProfile().userId()).orElseThrow(
-                        () -> new IllegalArgumentException("ID에 해당하는 유저가 없음"));
-                List<WhoMeet> metMales = whoMeetRepository.findByMetUser1(womanUser);
-                for (WhoMeet met : metMales) {
-                    metMalesSet.add(met.getMetUser2().getUserNickname());
-                }
-
-                for (Man man : menGroup.values()) {
-                    String manName = man.getName();
-                    if (metMalesSet.contains(manName)) {
-                        continue;
-                    }
-
-                    if (!woman.getUserPreferences().departmentPossible() && woman.getUserProfile().department().equals(man.getUserProfile().department())) {
-                        thirdPreferences.add(man.getName());
-                    } else if (man.getUserProfile().birthYear() >= woman.getUserPreferences().yearMin() && man.getUserProfile().birthYear() <= woman.getUserPreferences().yearMax()) {
-                        mainPreferences.add(man.getName());
-                    } else {
-                        secondaryPreferences.add(man.getName());
-                    }
-                }
-
-                mainPreferences.addAll(secondaryPreferences);
-                mainPreferences.addAll(thirdPreferences);
-                mainPreferences.addAll(metMalesSet);
-
-                woman.setPreferences(mainPreferences);
-
-                log.info("{}의 선호도 목록 : {}", woman.getName(), mainPreferences);
-            }
-
-            new GaleShapley(menGroup, womenGroup, roomRepository, userRepository, whoMeetRepository);
+            matchGroupsByMood(mood, menGroups.get(mood), womenGroups.get(mood));
         }
+    }
+
+    private void matchGroupsByMood(String mood, Map<String, Man> menGroup, Map<String, Woman> womenGroup) {
+        logGroupStart(mood);
+        logGroupMembers(mood, menGroup, womenGroup);
+
+        preparePreferences(menGroup, womenGroup);
+        preparePreferences(womenGroup, menGroup);
+
+        new GaleShapley(menGroup, womenGroup, roomRepository, userRepository, whoMeetRepository);
+    }
+
+    private void logGroupStart(String mood) {
+        log.info("====================================");
+        log.info("{} 그룹 매칭을 시작합니다.", mood);
+        log.info("====================================");
+    }
+
+    private void logGroupMembers(String mood, Map<String, Man> menGroup, Map<String, Woman> womenGroup) {
+        log.debug("{} 그룹 남자:", mood);
+        for (Man man : menGroup.values()) {
+            log.info(man.getName());
+        }
+
+        log.debug("{} 그룹 여자:", mood);
+        for (Woman woman : womenGroup.values()) {
+            log.info(woman.getName());
+        }
+    }
+
+    private <T extends Person, U extends Person> void preparePreferences(Map<String, T> group, Map<String, U> otherGroup) {
+        for (T person : group.values()) {
+            List<String> preferences = createPreferenceList(person, otherGroup);
+            person.setPreferences(preferences);
+
+            log.info("{}의 선호도 목록 : {}", person.getName(), preferences);
+        }
+    }
+
+    private <T extends Person, U extends Person> List<String> createPreferenceList(T person, Map<String, U> otherGroup) {
+        List<String> mainPreferences = new ArrayList<>();
+        List<String> secondaryPreferences = new ArrayList<>();
+        List<String> thirdPreferences = new ArrayList<>();
+        Set<String> metPeopleSet = getMetPeopleSet(person);
+
+        for (U otherPerson : otherGroup.values()) {
+            String otherPersonName = otherPerson.getName();
+            if (metPeopleSet.contains(otherPersonName)) continue;
+
+            if (isSameDepartment(person, otherPerson)) {
+                thirdPreferences.add(otherPersonName);
+            } else if (isWithinPreferredAgeRange(person, otherPerson)) {
+                mainPreferences.add(otherPersonName);
+            } else {
+                secondaryPreferences.add(otherPersonName);
+            }
+        }
+
+        mainPreferences.addAll(secondaryPreferences);
+        mainPreferences.addAll(thirdPreferences);
+        mainPreferences.addAll(metPeopleSet);
+
+        return mainPreferences;
+    }
+
+    private <T extends Person, U extends Person> boolean isSameDepartment(T person, U otherPerson) {
+        return !person.getUserPreferences().departmentPossible() &&
+                person.getUserProfile().department().equals(otherPerson.getUserProfile().department());
+    }
+
+    private <T extends Person, U extends Person> boolean isWithinPreferredAgeRange(T person, U otherPerson) {
+        return otherPerson.getUserProfile().birthYear() >= person.getUserPreferences().yearMin() &&
+                otherPerson.getUserProfile().birthYear() <= person.getUserPreferences().yearMax();
+    }
+
+    private <T extends Person> Set<String> getMetPeopleSet(T person) {
+        Set<String> metPeopleSet = new HashSet<>();
+        User user = userRepository.findById(person.getUserProfile().userId())
+                .orElseThrow(() -> new IllegalArgumentException("ID에 해당하는 유저가 없음"));
+
+        List<WhoMeet> metPeople;
+        if (person instanceof Man) {
+            metPeople = whoMeetRepository.findByMetUser2(user);
+            for (WhoMeet met : metPeople) {
+                metPeopleSet.add(met.getMetUser1().getUserNickname());
+            }
+        } else {
+            metPeople = whoMeetRepository.findByMetUser1(user);
+            for (WhoMeet met : metPeople) {
+                metPeopleSet.add(met.getMetUser2().getUserNickname());
+            }
+        }
+
+        return metPeopleSet;
     }
 
     private <T extends Person> Map<String, T> convertListToMap(List<T> list) {
@@ -170,15 +177,5 @@ public class MatchingService {
             groups.get(person.getMood()).put(person.getName(), person);
         }
         return groups;
-    }
-
-    private static <T extends Person> void printGroupedPersons(Map<String, Map<String, T>> groupedPersons) {
-        for (String mood : groupedPersons.keySet()) {
-            log.info("Mood: {}", mood);
-            Map<String, T> group = groupedPersons.get(mood);
-            for (T person : group.values()) {
-                log.info("Name: {}", person.getName());
-            }
-        }
     }
 }
