c8101d8e5f8c619a40e3b40eb4bfb8a577b1bcb5
DongHoon Lee <dhl1924@naver.com>
2024-05-26 19:44:31 +0900

    :bug:[fix]: prefer 테이블과 조인해서 mood를 가져오도록 수정

--- src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.moodmate.moodmatebe.domain.user.repository;
 
-import com.moodmate.moodmatebe.domain.user.domain.User;
 import com.moodmate.moodmatebe.domain.user.domain.Gender;
+import com.moodmate.moodmatebe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
     Optional<User> findByUserEmail(String userEmail);
-    List<User> findByUserNicknameAndUserGenderAndUserPreferMood(String nickname, Gender gender, String mood);
+
+    @Query("SELECT u FROM User u JOIN u.prefer p WHERE u.userNickname = :nickname AND u.userGender = :gender AND p.preferMood = :mood")
+    List<User> findByUserNicknameAndUserGenderAndUserPreferMood(@Param("nickname") String userNickname, @Param("gender") Gender userGender, @Param("mood") String userPreferMood);
 }
