f4846bfe36af43471c39c2d7ea6107d1ccd6c023
DongHoon Lee <dhl1924@naver.com>
2024-05-26 17:27:25 +0900

    :sparkles:[feat]: 같은 무드와 같은 성별일 때 닉네임 중복을 확인하는 메서드 추가

--- src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
+++ src/main/java/com/moodmate/moodmatebe/domain/user/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.moodmate.moodmatebe.domain.user.repository;
 
 import com.moodmate.moodmatebe.domain.user.domain.User;
+import com.moodmate.moodmatebe.domain.user.domain.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
     Optional<User> findByUserEmail(String userEmail);
+    List<User> findByUserNicknameAndUserGenderAndUserPreferMood(String nickname, Gender gender, String mood);
 }
